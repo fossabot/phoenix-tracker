a5eb37ba50574a27f462fb5a0e5150d7201bddf1
Mugdha Arunim Ahmed <mugdhaarunimahmed2017@gmail.com>
2021-12-23 16:38:03 +0600

    migrated to react-native-firebase

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,18 +2,15 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { Provider } from 'react-native-paper';
 import theme from '../common/theme';
-import FirebaseProvider from './firebase';
 
 export default function App(): JSX.Element {
   return (
-    <FirebaseProvider>
-      <Provider theme={theme}>
-        <View style={styles.container}>
-          <Text>Open up App.tsx to start working on your app!</Text>
-          <StatusBar style="auto" />
-        </View>
-      </Provider>
-    </FirebaseProvider>
+    <Provider theme={theme}>
+      <View style={styles.container}>
+        <Text>Open up App.tsx to start working on your app!</Text>
+        <StatusBar style="auto" />
+      </View>
+    </Provider>
   );
 }
 
